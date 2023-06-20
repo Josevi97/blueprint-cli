007fadada9989649e5a901b3017cd39607afd9f8
josevi97 <josevi00@hotmail.com>
2023-06-20 20:59:22 +0200

    feat: help process now has more descriptible info

--- modules/help/process_impl.go
+++ modules/help/process_impl.go
@@ -10,23 +10,24 @@ type HelpProcess struct{}
 var Log = logging.NewLogging("HELP PROCESS")
 
 func args(command string, msg string) {
-	Log.Output("%s\t%s", command, msg)
+	Log.Output("%s\t\t%s", command, msg)
 }
 
 func (helpProcess *HelpProcess) Execute() {
 	Log.Output("Blueprint CLI for structuring your project defining conventions.")
-	Log.Output("The basic CLI commands are as follows:")
-	args(process.HELP, "\tdisplays the blueprint CLI commands available")
-	args(process.INIT, "\tuse this command to initialize blueprint CLI")
+	Log.Output("The basic CLI commands are as follows:\n")
+
+	args(process.HELP, "\tdisplays the blueprint CLI commands available\n")
+	args(process.INIT, "\tuse this command to initialize blueprint CLI\n")
 	args(process.REGISTER, "registers a block structure in order to get a command to generate a block item")
-	args("  --command -c", "dictamines the command to be used for generate command\n")
+	args("  -c --command", "dictamines the command to be used for generate command\n")
 
-	args(process.GENERATE, "generates a block item depending on the blocks registered")
-	args(process.LIST, "\tlists every command block generated")
-	args(process.BLUEPRINT, "opens GUI blueprint")
+	args(process.GENERATE, "generates a block item depending on the blocks registered\n")
+	args(process.LIST, "\tlist every command block generated\n")
+	args(process.BLUEPRINT, "opens GUI blueprint\n")
 
-	Log.Output("\nCommon flags are as follows:")
-	args("--log -l", "show logs also on the standard output and errput")
+	Log.Output("\nCommon flags are as follows:\n")
+	args("-l --log", "show logs also on the standard output and errput\n")
 
 	Log.Info("executed successfully")
 }
